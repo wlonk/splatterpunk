e5b4b94b144197a8261874fd27c3ab0869244ae6
Kit La Touche <kit.la.t@gmail.com>
2013-11-17 13:55:11 -0700

    Added support for emailing errors.

--- splatterpunk/splatterpunk/settings/local.py
+++ splatterpunk/splatterpunk/settings/local.py
@@ -6,3 +6,8 @@ TEMPLATE_DEBUG = False
 INSTALLED_APPS += (
     'django_nose',
 )
+
+########## EMAIL CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+########## END EMAIL CONFIGURATION
--- splatterpunk/splatterpunk/settings/production.py
+++ splatterpunk/splatterpunk/settings/production.py
@@ -1,1 +1,14 @@
 from .base import *
+
+
+########## EMAIL CONFIGURATION
+# See: https://docs.djangoproject.com/en/dev/ref/settings/#email-backend
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = get_env_variable('EMAIL_HOST')
+EMAIL_HOST_PASSWORD = get_env_variable('EMAIL_HOST_PASSWORD')
+EMAIL_HOST_USER = get_env_variable('EMAIL_HOST_USER')
+EMAIL_PORT = get_env_variable('EMAIL_PORT')
+EMAIL_SUBJECT_PREFIX = '[%s] ' % SITE_NAME
+EMAIL_USE_TLS = True
+SERVER_EMAIL = EMAIL_HOST_USER
+########## END EMAIL CONFIGURATION
