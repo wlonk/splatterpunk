348f3833301da61eacf8e33eadc86c4955c70b6b
Kit La Touche <kit.la.t@gmail.com>
2013-11-17 16:31:41 -0700

    Added Heroku security-header settings.

--- splatterpunk/splatterpunk/settings/base.py
+++ splatterpunk/splatterpunk/settings/base.py
@@ -57,6 +57,8 @@ ALLOWED_HOSTS = [
     '{project}.herokuapp.com.'.format(project=PROJECT_NAME),
 ]
 
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Application definition
 
