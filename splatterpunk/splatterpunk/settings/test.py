db00cae03bd9299f585f02d81a50b848b6d930cf
Kit La Touche <kit.la.t@gmail.com>
2013-11-18 09:08:17 -0700

    Updated Travis config.
    Now it should provision a database for use in tests.

--- splatterpunk/splatterpunk/settings/test.py
+++ splatterpunk/splatterpunk/settings/test.py
@@ -3,3 +3,9 @@ from .base import *
 INSTALLED_APPS += (
     'django_nose',
 )
+
+DATABASES['default'] = dj_database_url.config(
+    default='postgres://postgres@localhost/{project}'.format(
+        project=PROJECT_NAME
+    )
+)
